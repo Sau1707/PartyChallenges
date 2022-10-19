fdc257f06711649bd74244c255872b29aa15b294
Sau1707 <leonardo@saurwein.ch>
2022-10-19 17:58:06 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  basePath: process.env.NODE_ENV == "development" ? null : "/PartyChallenges"
 }
 
 module.exports = nextConfig
