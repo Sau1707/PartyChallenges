a585fb53d83ac2e4baa5462a1c6df2f3c14c4f4d
Sau1707 <Sau1707@users.noreply.github.com>
2022-10-20 00:05:19 +0000

    deploy: a708a2f3322a583aa45d8b6667908f3378580e7e

---
+++ _next/static/QWhMYzrhiQbsU4EPJboC1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/7028fb2c4055f96c.css","static/chunks/pages/index-df97926c9017e029.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
